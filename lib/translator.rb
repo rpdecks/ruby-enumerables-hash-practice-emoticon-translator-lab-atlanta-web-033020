fc7a23aea8f4a6f0da03d8ec2677eb06ee4876f9
Robert Phillips <rpdecks@gmail.com>
2020-03-22 15:05:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(file)
   }
   emoticons.each do |eng_meaning|
     eng_meaning.each do |jap_icons_array, index|
-      puts jap_icons_array[index]
+      puts jap_icons_array
       binding.pry
       my_hash[:get_meaning][index] = {}
     end
